0e7ddde78756d25fc636b7a649ed80262df882f8
jihchi <achi@987.tw>
2021-02-20 17:23:00 +0800

    Add a minimum working test

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -2,5 +2,12 @@
 process.env.NODE_ENV = 'test';
 
 module.exports = {
+  testRunnerHtml: (testFramework) =>
+    `<html>
+      <body>
+        <script>window.app = { backend: 'http://web-test-runner' }</script>
+        <script type="module" src="${testFramework}"></script>
+      </body>
+    </html>`,
   plugins: [require('@snowpack/web-test-runner-plugin')()],
 };
